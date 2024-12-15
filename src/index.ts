7499353fcd49c9b64cdc1a9e7903b50d8e3421ac
Oscar Ballesteros <oscartic@gmail.com>
2024-12-14 23:51:23 -0300

    add logs colors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import colors from 'colors';
 import server from './server';
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(colors.magenta.bold(`Server is running on http://localhost:${port}`));
 });
