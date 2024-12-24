a575ca6171d73ece68f5d97a5ce5273196c70aab
Oscartic <oscartic@gmail.com>
2024-12-24 00:14:33 -0300

    Feat/user schema (#2)
    * User model and router save ep
    
    * add handlers
    
    * install package
    
    * add handler to user model
    
    * utils for manage passwords
    
    * params validation
    
    * user handler

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,36 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { createAccount, login } from './handlers';
 
 const router = Router();
 
 //Routing 
-router.post('/auth/register', (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
+router.post('/auth/register',
+  body('handle')
+    .notEmpty()
+    .withMessage('Handle is required'),
+  body('name')
+    .isEmail()
+    .withMessage('Name is required'),
+  body('email')
+    .isEmail()
+    .withMessage('Email most be a valid email address'),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('Password is too short (min 6 characters)'),
+  createAccount,
+);
+
+router.post('/auth/login',
+  body('email')
+    .isEmail()
+    .withMessage('Email most be a valid email address'),
+  body('password')
+    .notEmpty()
+    .isLength({ min: 6 })
+    .withMessage('Password is required)'),
+  login
+);
 
 
 export default router;
