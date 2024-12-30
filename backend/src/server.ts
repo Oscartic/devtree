c9f3f5ec17eaabe0e62bf703c29450f049824429
Oscar Ballesteros <oscartic@gmail.com>
2024-12-30 20:15:53 -0300

    [backend] feat (server/cors): add cors config rules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import  router from './router';
 import { connectDB } from './config/db';
 import 'dotenv/config';
-
-const app = express();
+import { corsConfig } from './config/cors';
 
 connectDB();
+
+const app = express();
+// Cors
+app.use(cors(corsConfig));
+
 // read form data
 app.use(express.json());
 
