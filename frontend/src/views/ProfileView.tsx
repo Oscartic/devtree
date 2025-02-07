4855d70acee42012f9650936f7bf243da6108899
Oscar Ballesteros <oscartic@gmail.com>
2025-02-06 23:59:16 -0300

    [frontend] add mutation and show image

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -3,7 +3,7 @@ import { useQueryClient, useMutation } from '@tanstack/react-query';
 import { toast } from 'sonner';
 import ErrorMessage from '../components/ErrorMessage';
 import { User, ProfileForm } from '../types';
-import { updateProfile } from '../api/DevTreeAPI';
+import { updateProfile, uploadImage } from '../api/DevTreeAPI';
 
 export default function ProfileView() {
 
@@ -27,6 +27,27 @@ export default function ProfileView() {
         }
     });
 
+    const uploadImageMutation = useMutation({
+        mutationFn: uploadImage,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            console.log(data);
+            queryClient.setQueryData(['user'], (prevData: User) => {
+                return {
+                    ...prevData,
+                    image: data.image
+                }    
+            }); // Optimistic Update
+        }
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.files) {
+            uploadImageMutation.mutate(e.target.files[0]);
+        }
+    }
     const handleUserProfileForm = (formData: ProfileForm) => {
         updateProfileMutation.mutate(formData);
     };
@@ -79,7 +100,7 @@ export default function ProfileView() {
                     name="handle"
                     className="border-none bg-slate-100 rounded-lg p-2"
                     accept="image/*"
-                    onChange={ () => {} }
+                    onChange={ handleChange }
                 />
             </div>
 
